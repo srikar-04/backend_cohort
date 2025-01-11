ebb9de2c31d680a5d019a7514ebe04ed91e40e1e
srikar <srikar.code01@gmail.com>
2025-01-11 17:37:44 +0530

    completed todo crud

--- 01_basics/01_todoAppBackend.js
+++ 01_basics/01_todoAppBackend.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const todos = [
+let todos = [
     {
         id: 1,
         content: 'go to gym'
@@ -24,12 +24,31 @@ app.post('/create', function(req, res) {
     const content = req.body.content
     if(content) {
         todos.push({id:todos.length+1, content: content})
-        console.log(todos);
         res.json({msg:'create operation done'})
     } else {
         res.json({msg:'cannot do create operation, something is wrong'})
     }
 
+})
+
+app.put('/update', function(req,res) {
+    const n = req.query.n
+    const content = req.body.content
+    todos = todos.map(todo => {
+        if(todo.id != n) return todo
+        else return {...todo, content}
+    })
+    res.json({msg: 'update operation done'})
+})
+
+app.delete('/delete', function(req, res) {
+    const n = req.query.n;
+    if(!n) {
+        res.json({msg:'param not foung'})
+    }
+    todos = todos.filter(todo => todo.id != n)
+
+    res.json({msg:'delete operation done'})
 })
 
 app.listen(3000)
