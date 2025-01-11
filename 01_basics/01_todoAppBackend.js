00e5a65eff5096c195915fa719bd680c9b1154d9
srikar <srikar.code01@gmail.com>
2025-01-11 15:18:20 +0530

    post and updates get method

--- 01_basics/01_todoAppBackend.js
+++ 01_basics/01_todoAppBackend.js
@@ -16,16 +16,20 @@ const app = express()
 app.use(express.json())
 
 app.get('/', function(req, res) {
-    res.send(
-        [
-            'crud operation => todo-app',
-            {
-                create: '/create',
-                update: '/update',
-                delete: '/delete'
-            }
-        ]
-    )
+    // res.send(todos.map(todo => todo.content))
+    res.send(todos)
+})
+
+app.post('/create', function(req, res) {
+    const content = req.body.content
+    if(content) {
+        todos.push({id:todos.length+1, content: content})
+        console.log(todos);
+        res.json({msg:'create operation done'})
+    } else {
+        res.json({msg:'cannot do create operation, something is wrong'})
+    }
+
 })
 
 app.listen(3000)
