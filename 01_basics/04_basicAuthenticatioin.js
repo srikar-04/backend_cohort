a372a31a0edbdfb8ddc0eb8c7632c75df8c0d1e5
srikar <srikar.code01@gmail.com>
2025-01-13 11:34:37 +0530

    added signin, signup routes and tokenGenrator

--- 01_basics/04_basicAuthenticatioin.js
+++ 01_basics/04_basicAuthenticatioin.js
@@ -4,12 +4,61 @@ const app = express();
 
 app.use(express.json())
 
+const users = [];
+
+const tokenGenerator = () => {
+    const string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let token = '';
+    for(let i = 0; i < string.length; i++) {
+        token += string[Math.floor(Math.random()*string.length)]
+    }
+    return token
+}
+
+app.get('/', function(req, res) {
+    res.send('this is basic authentication')
+})
+
 app.post('/signup', function(req, res) {
-    res.json({msg:'you are singedup'})
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let isPresent = false;
+
+    users.map(user => {
+        if(user.username === username && user.password === password) isPresent = true
+    } )
+
+    if(!isPresent) {
+        users.push({username: username, password: password})
+        res.json({msg:'you are singed up'})
+    } else {
+        res.send('user already exsists')
+    }
+    console.log(users);
 })
 
 app.post('/signin', function(req, res) {
-    res.json({msg:'you are signed in'})
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let isSignedIn = false;
+
+    users.map(user => {
+        if(user.username === username && user.password === password) {
+            const token = tokenGenerator()
+            user.token = token
+            isSignedIn = true
+        }
+    })
+    if(isSignedIn) {
+        res.json({msg:'you are signed in'})
+    } else {
+        res.send('invalid username or password')
+    }
+
+    console.log(users);
 })
 
 
