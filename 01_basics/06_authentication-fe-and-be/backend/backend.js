b8f8c50d87cdef5b53d325f1cbd0ded602da4192
srikar <srikar.code01@gmail.com>
2025-01-13 22:37:24 +0530

    added bakcend for basic sigup-signin page

---
+++ 01_basics/06_authentication-fe-and-be/backend/backend.js
@@ -0,0 +1,78 @@
+// LECTURE => 6.2 | Auth And Connecting FE To BE
+
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = 'randomString';
+
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+app.post('/signup', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let isPresent = false;
+
+    users.map(user => {
+        if(user.username === username && user.password === password) isPresent = true
+    } )
+
+    if(!isPresent) {
+        users.push({username: username, password: password})
+        res.json({msg:'you are singed up'})
+    } else {
+        res.send('user already exsists')
+    }
+    console.log(users);
+})
+
+app.post('/signin', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // let isPresent = false;
+    let user = null
+
+    user = users.find(user => user.username === username && user.password === password)
+
+    let token = null
+
+    if(user) {
+        token = jwt.sign({
+            username:username
+        }, JWT_SECRET)
+        user.token = token
+        res.json({
+            msg: 'you are sucesfully singed in',
+            token: token,
+        })
+    } else {
+        res.send('user not found. Incorrect username or password')
+    }
+    console.log(users);
+})
+
+app.get('/me', function(req, res) {
+    const token = req.headers.token
+    let info = null
+    info = jwt.verify(token, JWT_SECRET);
+
+    if(info) {
+        users.map(user => {
+            if(user.username === info.username) {
+                res.json({
+                    username: user.username,
+                    password:user.password,
+                    token: user.token
+                })
+            }
+        })
+    } else {
+        res.send('invalid token')
+    }
+})
+
+app.listen(3000);
