fb0ae4cc0beab04a86094088ded9fb5b212ce236
srikar <srikar.code01@gmail.com>
2025-02-02 21:49:54 +0530

    feat: enhance Todo and User models with additional fields and validation

--- 02_mogoDb_basics/models/todo.models.js
+++ 02_mogoDb_basics/models/todo.models.js
@@ -1,7 +1,17 @@
 import mongoose, {Schema} from "mongoose";
 
+//userId, completed, todo, 
 const todoSchema = new Schema({
-    
+    completed: {
+        type: Boolean
+    },
+    todo: {
+        type: String
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
 }, {timestamps: true})
 
 export const Todo = mongoose.model("Todo", todoSchema)
--- 02_mogoDb_basics/models/user.models.js
+++ 02_mogoDb_basics/models/user.models.js
@@ -1,7 +1,20 @@
 import mongoose, {Schema} from "mongoose";
 
+// username, password, email
 const userSchema = new Schema({
-
+    username: {
+        type: String,
+        required: [true, 'username is required'],
+    },
+    password: {
+        type: String,
+        required: [true, 'password is required'],
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: [true, 'email is required']
+    }
 }, {timestamps: true})
 
 export const User = mongoose.model("User", userSchema)
