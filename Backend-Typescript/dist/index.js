dc01834ccb0defda8f33c14603629acce1032d67
srikar-04 <srikar.code01@gmail.com>
2025-08-26 08:31:55 +0530

    compiled ts

---
+++ Backend-Typescript/dist/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World from Express!');
+});
+app.get('/hello', (req, res) => {
+    res.send('Hello from the hello route');
+});
+app.use(express.json());
+app.get('/test', (req, res) => {
+    res.send('from the test route');
+});
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+//# sourceMappingURL=index.js.map
