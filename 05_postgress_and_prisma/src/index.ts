7a32c71a6816a70b13ff527e6eb3ccce53ae18be
srikar-04 <srikar.code01@gmail.com>
2026-01-21 12:09:12 +0530

    feat: Add PostgreSQL client setup with basic select and insert operations for users and posts.

--- 05_postgress_and_prisma/src/index.ts
+++ 05_postgress_and_prisma/src/index.ts
@@ -7,8 +7,20 @@ async function main() {
     await pgClient.connect()
 
     const response = await pgClient.query('SELECT * FROM users;')
+    const response2 = await pgClient.query('SELECT * FROM posts;')
 
-    console.log(response.rows)
+    const response3 = await pgClient.query('SELECT email, title FROM users JOIN posts ON users.id = posts.user_id')
+
+    const response4 = await pgClient.query(`
+        INSERT INTO posts (user_id, title)
+        VALUES ($1, $2)
+        RETURNING id, user_id, title, created_at
+    `, ['1', 'third post of first user'])
+
+    // console.log(response.rows)
+    // console.log(response2.rows)
+    // console.log(response3.rows)
+    // console.log(response4.rows)
 }
 
 main()
